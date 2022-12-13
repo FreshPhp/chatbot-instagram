9752b016a5aa36fe22d5c11d919e9ae905dcf220
@Fresh Programing <93295405+FreshPhp@users.noreply.github.com>
2022-12-13 11:10:50 -0300

    Update fresh.js

--- fresh.js
+++ fresh.js
@@ -1,19 +1,18 @@
 const get = require('@androz2091/insta.js');
-const moment = require('moment-timezone')
-const time = moment().format('HH:mm:ss')
-const { color } = require('color.js')
-const { performance } = require('perf_hooks')
-const { getBuffer } = require('¿.js');
+const moment = require('moment-timezone');
+const time = moment().format('HH:mm:ss');
+const { color } = require('color.js');
+const { performance } = require('perf_hooks');
+const { getBuffer } = require('index.js');
 const fs = require("fs");
 const fetch = require('node-fetch');
 const request = require('request');
 const yts = require("yt-search");
 const fresh = new get.Client();
 
-//========== [ //Conexão\\ ] ==========\\
 
 fresh.on('connected', () => {
-console.log('\x1b[1;34m~\x1b[1;37m>>', '[\x1b[1;33mINF\x1b[1;37m]', time, color(`\nConectado na conta`), color(`${fresh.user.username}\n`))
+console.log('\x1b[1;34m~\x1b[1;37m>>', '[\x1b[1;33mINF\x1b[1;37m]', time, color('\nConectado na conta'), color(fresh.user.username+'\n'))
 });
  
 fresh.on('pendingRequest', fresh => {
@@ -36,25 +35,12 @@ testeq = `
  return await connect.reply(testeq)
  }
  
- if (RegExp('/menu',"i").exec(text)){
-connect.chat.startTyping(5000, true) 
-test = `
-┏─━──━──━──━─
-┃「BOT BETA INSTAGRAM」
-┃「MEU CRIADOR: Wa.me/+5515988171796」
-┃「BOT DO WPP: Wa.me/+5515988306514」
-┃「MEU SITE: https://fresh-php.herokuapp.com」
-┃ BY: Fresh Programing ©
-┗─━──━──━──━─
-=> /cassino`
- return await connect.reply(test)
- }
  
  
  if (RegExp('/cassino',"i").exec(text)){
  	connect.reply('\nRODANDO...')
  	connect.chat.startTyping(10000, true) 
-const soto = [
+const fruits = [
 '🍊 : 🍒 : 🍐',
 '🍒 : 🔔 : ??',
 '🍇 : 🍇 : 🍇',
@@ -76,8 +62,8 @@ const soto = [
 '🍒 : 🍒 : 🍒',
 '🍌 : 🍌 : 🍌'
 ]		
-const somtoy2 = soto[Math.floor(Math.random() * soto.length)]
-if ((somtoy2 == '🥑 : 🥑 : 🥑') ||(somtoy2 == '🍉 : 🍉 : 🍉') ||(somtoy2 == '🍓 : 🍓 : 🍓') ||(somtoy2 == '🍎 : 🍎 : 🍎') ||(somtoy2 == '🍍 : 🍍 : 🍍') ||(somtoy2 == '🥝 : 🥝 : 🥝') ||(somtoy2 == '🍑 : 🍑 : 🍑') ||(somtoy2 == '🥥 : 🥥 : 🥥') ||(somtoy2 == '🍋 : 🍋 : 🍋') ||(somtoy2 == '🍐 : 🍐 : 🍐') ||(somtoy2 == '🍌 : 🍌 : 🍌') ||(somtoy2 == '🍒 : 🍒 : 🍒') ||(somtoy2 == '🔔 : 🔔 : 🔔') ||(somtoy2 == '🍊 : 🍊 : 🍊') ||(somtoy2 == '?? : 🍇 : 🍇')) {
+const random = fruits[Math.floor(Math.random() * fruits.length)]
+if ((random == '🥑 : 🥑 : 🥑') ||(random == '🍉 : 🍉 : 🍉') ||(random == '🍓 : 🍓 : 🍓') ||(random == '🍎 : 🍎 : 🍎') ||(random == '🍍 : 🍍 : 🍍') ||(random == '🥝 : 🥝 : 🥝') ||(random == '🍑 : 🍑 : 🍑') ||(random == '🥥 : 🥥 : 🥥') ||(random == '🍋 : 🍋 : 🍋') ||(random == '🍐 : 🍐 : 🍐') ||(random == '🍌 : 🍌 : 🍌') ||(random == '🍒 : 🍒 : 🍒') ||(random == '🔔 : 🔔 : 🔔') ||(random == '🍊 : 🍊 : 🍊') ||(random == '?? : 🍇 : 🍇')) {
 var Vitória = "\nVocê ganhou!!"
 } else {
 var Vitória = "\nVocê perdeu!"
@@ -86,7 +72,7 @@ var Vitória = "\nVocê perdeu!"
 let fresh =
 `
 ┏━━━━❪🎰❫━━━━
-┣►${somtoy2}◄┛
+┣►${random}◄┛
 ┗━━━━❪★❫━━━━
 
 ${Vitória}`
@@ -101,6 +87,6 @@ return await connect.reply(fresh)
 });
 
 
-//Obs no lugar de 'nome da conta' coloque o arroba do perfil do Instagram que você quer que o bot seja conectado, e logo do lado a senha da mesma conta 
-fresh.login('fresh_bot.js', 'fresh789811');
+//Obs no lugar de 'nome da conta' coloque o arroba do perfil do Instagram que você quer que o bot seja conectado, e logo do lado coloque a senha. 
+fresh.login('Nome da conta', 'senha');
 
